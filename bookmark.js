6575c20d363780e737ba1822b2728a0eb304a702
NeutralDread <jordanamiles@hotmail.com>
2018-10-17 13:49:05 -0500

    Added alert functions if not proper Name+Url

--- bookmark.js
+++ bookmark.js
@@ -8,16 +8,7 @@ function saveBookmark(e) {
   var siteName = document.getElementById('siteName').value;
   var siteUrl = document.getElementById('siteUrl').value;
 
-  if (!siteName || !siteUrl) {
-    alert("Please fill in the form");
-    return false;
-  }
-
-  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-  var regex = new RegExp(expression);
-
-  if (!siteUrl.match(regex)) {
-    alert("Please use a vailid URL");
+  if (!validateForm(siteName + siteUrl)) {
     return false;
   }
 
@@ -25,6 +16,7 @@ function saveBookmark(e) {
     name: siteName,
     url: siteUrl
   }
+
   /*
       // Local storage test
       localStorage.setItem('Test', 'Hello Wolrd');
@@ -94,4 +86,21 @@ function fetchBookmarks() {
       '</h3>' +
       '</div>';
   }
+}
+
+function validateForm(siteName, siteUrl) {
+  if (!siteName || !siteUrl) {
+    alert("Please fill in the form");
+    return false;
+  }
+
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if (!siteUrl.match(regex)) {
+    alert("Please use a vailid URL");
+    return false;
+  }
+
+  return true;
 }
